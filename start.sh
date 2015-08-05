8bc1f0cb46f32af9e4fbd38c56a9a764c25ee580
Deven Phillips <deven.phillips@gmail.com>
2015-08-05 17:28:21 -0400

    Fixed a few issues

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-if ! [ -d /data/conf/puppetserver ]; then
+if ! [ -d /data/conf ]; then
     printf "Initializing config because no configuration was found\n"
+    mkdir -p /data/conf
     cp -fr /etc/puppetlabs/* /data/conf
 fi
 
@@ -18,6 +19,15 @@ fi
 printf "Linking mounted logs directory\n"
 rm -rf /var/log
 ln -s /data/log /var/log
+
+printf "Linking executables into path\n"
+cd /opt/puppetlabs/puppet/bin
+for I in *
+do
+    if ! [ -f /usr/bin/${I} ]; then
+        ln -s /opt/puppetlabs/puppet/bin/${I} /usr/bin/${I}
+    fi
+done
 
 printf "Running supervisord to start puppetserver\n"
 exec supervisord -n -c /etc/supervisor/supervisord.conf
